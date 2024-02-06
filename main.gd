2f317026406bf700053ab65e687723b3ce64ad69
Trent Waddington <trent.waddington@gmail.com>
2024-02-06 13:25:01 +1000

    attempt passthrough

--- main.gd
+++ main.gd
@@ -5,12 +5,14 @@ var xr_interface: XRInterface
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
-		print("OpenXR initialized successfully")
-
-		# Turn off v-sync!
-		#DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-
-		# Change our main viewport to output to the HMD
+		if xr_interface.is_passthrough_supported():
+			get_viewport().transparent_bg = true
+			#await get_tree().create_timer(1.3).timeout
+			#xr_interface.stop_passthrough()
+			print(xr_interface.start_passthrough())
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+
+func _process(_p):
+	pass
